46c10d00b875e3467c72e3a12436d37ee4dfaf61
Aleksey Uimanov <s9gf4ult@gmail.com>
2017-02-22 02:02:18 +0500

    now we can fold our data with very hard and unobvious way.

--- burn/src/Peridot/TimeSeries.hs
+++ burn/src/Peridot/TimeSeries.hs
@@ -1,5 +1,11 @@
 module Peridot.TimeSeries where
 
+import Control.Foldl as FL
+import Data.Dependent.Map as DM
+import Data.GADT.Compare
+import Data.GADT.Compare.TH
+import Data.Singletons
+import Data.Singletons.Prelude (Sing(..))
 import Data.Singletons.TH
 import Data.Time
 import Peridot.Types
@@ -9,20 +15,20 @@ singletons [d|
       = OverLen
       | OverStart
 
-    data Statistis
+    data Statistics
       = Count
       | Min Over
       | Max Over
       | Sum Over|]
 
-type StatisticsKey = Sing Statistis
+type StatisticsKey = Sing Statistics
 type OverKey = Sing Over
 
 data OverValue (a :: Over) where
   OverLenValue :: NominalDiffTime -> OverValue 'OverLen
   OverStartValue :: UTCTime -> OverValue 'OverStart
 
-data StatisticsValue (a :: Statistis) where
+data StatisticsValue (a :: Statistics) where
   CountValue :: Int -> StatisticsValue 'Count
   MinValue :: OverValue over -> StatisticsValue ('Min over)
   MaxValue :: OverValue over -> StatisticsValue ('Max over)
@@ -32,7 +38,7 @@ data StatisticsValue (a :: Statistis) where
 data TimeSeries a
   = Start
   | Len
-  | TSStatistics Statistis
+  | TSStatistics Statistics
   | Payload a
 
 -- | Instnatiate this TF for your extended data to define key
@@ -48,8 +54,28 @@ data TimeSeriesKey (a :: TimeSeries rest) where
   TSStatisticsKey :: Sing stat -> TimeSeriesKey ('TSStatistics stat)
   PayloadKey      :: Key rest -> TimeSeriesKey ('Payload rest)
 
+instance GEq TimeSeriesKey -- FIXME: implement
+instance GCompare TimeSeriesKey -- FIXME: implement
+
 data TimeSeriesValue (a :: TimeSeries rest) where
   StartValue        :: UTCTime              -> TimeSeriesValue 'Start
   LenValue          :: NominalDiffTime      -> TimeSeriesValue 'Len
   TSStatisticsValue :: StatisticsValue stat -> TimeSeriesValue ('TSStatistics stat)
   PayloadValue      :: Value rest           -> TimeSeriesValue ('Payload rest)
+
+type TSRecord = Record TimeSeriesKey TimeSeriesValue
+type TSRecFolder = RecFolder TimeSeriesKey TimeSeriesValue
+type TSGrouper key = RecGrouper key TimeSeriesKey TimeSeriesValue
+
+mkStatFoldl :: Sing (stat :: Statistics) -> FL.Fold TSRecord TSRecord
+mkStatFoldl = \case
+  SCount -> (DM.singleton (TSStatisticsKey SCount) . TSStatisticsValue . CountValue) <$> FL.length
+  -- FIXME: implement the rest of constructors
+
+squashStatFoldl :: Sing (stat :: [Statistics]) -> FL.Fold TSRecord TSRecord
+squashStatFoldl = \case
+  SCons stat rest -> DM.union <$> mkStatFoldl stat <*> squashStatFoldl rest
+  SNil            -> pure $ DM.empty
+
+tsRecordFolder :: Sing (stat :: [Statistics]) -> TSRecFolder
+tsRecordFolder stat = FL.fold (squashStatFoldl stat)
