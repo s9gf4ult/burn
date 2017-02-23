a3ff9fbbaea08613bf0556711e94ee57c32d26f6
Aleksey Uimanov <s9gf4ult@gmail.com>
2017-02-23 13:54:49 +0500

    wip making time series for peridot

--- burn/src/Peridot/TimeSeries.hs
+++ burn/src/Peridot/TimeSeries.hs
@@ -5,7 +5,9 @@ import Data.Dependent.Map as DM
 import Data.Functor.Identity
 import Data.GADT.Compare
 import Data.GADT.Compare.TH
+import Data.Profunctor
 import Data.Time
+import Data.Vinyl
 import Peridot.Types
 
 data StatAgg
@@ -41,31 +43,40 @@ type TSRecord rest = Record (TimeSeries rest) Identity
 type TSRecFolder rest = RecFolder (TimeSeries rest) Identity
 type TSGrouper rest = RecGrouper (TimeSeries rest) Identity
 
+-- | Foldl which can throw error
+type ErrFold a b = FL.FoldM (Either String) a b
 
-aggFold :: StatAgg -> FL.Fold a a
-aggFold = (error "FIXME: ")
+lmapM :: (Monad m) => (a -> m b) -> FL.FoldM m b r -> FL.FoldM m a r
+lmapM amb (FoldM step start end) = FoldM step' start end
+  where
+    step' x a = do
+      b <- amb a
+      step x b
+
+aggFold :: (Num a) => StatAgg -> FL.Fold a a
+aggFold = \case
+  StatSum -> FL.sum
 
 mkStatFoldl
-  :: Statistics rest val
-  -> Either String (FL.Fold (TSRecord rest) (TSRecord rest))
+  :: (GCompare rest, Num val)
+  => Statistics rest val
+  -> ErrFold (TSRecord rest) (TSRecord rest)
 mkStatFoldl = \case
-  StatisticsCount -> Right $ fmap toRecord $ FL.length
+  StatisticsCount -> generalize $ fmap toRecord $ FL.length
     where
       toRecord i = DM.singleton (Stats StatisticsCount) $ Identity i
-  key@(StatisticsAgg agg ts) -> traverse aggRecord $ aggFold agg
+  key@(StatisticsAgg agg ts) ->
+    lmapM fromRec $ generalize $ fmap toRec $ aggFold agg
     where
-      aggRecord r = case DM.lookup ts r of
-        Nothing -> Left "Not found key" -- FIXME: make errors usable
-        Just v -> Right $ DM.singleton (Stats key) $ Identity v
+      fromRec r = case DM.lookup ts r of
+        Nothing -> Left "key not found" -- FIXME: usefull error
+        Just v  -> Right $ runIdentity v
+      toRec v = DM.singleton (Stats key) $ Identity v
 
-
-  -- SCount -> (DM.singleton (TSStatisticsKey SCount) . TSStatisticsValue . CountValue) <$> FL.length
-  -- -- FIXME: implement the rest of constructors
-
--- squashStatFoldl :: Sing (stat :: [Statistics]) -> FL.Fold TSRecord TSRecord
--- squashStatFoldl = \case
---   SCons stat rest -> DM.union <$> mkStatFoldl stat <*> squashStatFoldl rest
---   SNil            -> pure $ DM.empty
-
--- tsRecordFolder :: Sing (stat :: [Statistics]) -> TSRecFolder
--- tsRecordFolder stat = FL.fold (squashStatFoldl stat)
+squashStatFoldl
+  :: (GCompare rest)
+  => Rec (Statistics rest) vals
+  -> ErrFold (TSRecord rest) (TSRecord rest)
+squashStatFoldl = \case
+  stat :& rest -> DM.union <$> mkStatFoldl stat <*> squashStatFoldl rest
+  RNil         -> pure DM.empty
