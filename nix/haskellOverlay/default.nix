316740fa1227eb17d4bdb6a862b3331639ef1bef
Aleksey Uymanov <s9gf4ult@gmail.com>
2023-01-24 12:47:34 +0600

    fix names

--- nix/haskellOverlay/default.nix
+++ nix/haskellOverlay/default.nix
@@ -2,14 +2,14 @@
 
 let
   cabal2nix = "${pkgs.cabal2nix}/bin/cabal2nix" ;
-  genDefault = path: pkgs.runCommand "dotnix" {} ''
+  genDerivation = path: pkgs.runCommand "derivation.nix" {} ''
     ${cabal2nix} ${path} > $out
   '' ;
 in
 with haskellLib; {
-  burn = super.callPackage (genDefault ../../burn) {} ;
-  burn-cli = super.callPackage (genDefault ../../burn-cli) {} ;
-  burn-gtk = super.callPackage (genDefault ../../burn-gtk) {} ;
+  burn = super.callPackage (genDerivation ../../burn) {} ;
+  burn-cli = super.callPackage (genDerivation ../../burn-cli) {} ;
+  burn-gtk = super.callPackage (genDerivation ../../burn-gtk) {} ;
 
   gtk3 = super.callPackage ./gtk3.nix { inherit (pkgs) gtk3; };
   cairo = super.callPackage ./cairo.nix { inherit (pkgs) cairo; };
